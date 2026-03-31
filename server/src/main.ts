a1a2ab86020e88feffb3c2a11fe943a5de2e536a
Mayur K Setty <settymayurk@gmail.com>
2026-03-31 21:15:04 +0530

    prisma config

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Server is running on port', process.env.PORT);
+  console.log('Server is running on port', process.env.PORT, "access it at http://localhost:" + process.env.PORT);
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
