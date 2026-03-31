0d046270db3e1b0b5f1f56ca29853b9184406891
Mayur K Setty <settymayurk@gmail.com>
2026-03-31 14:20:40 +0530

    added more to home page

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log('Server is running on port', process.env.PORT);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
